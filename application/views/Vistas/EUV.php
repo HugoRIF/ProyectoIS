1408330aa5d342f0356dd80f984689d09e65a7e9
Hugo Icelo <hugoicelo@MacBook-Pro-de-Hugo.local>
2018-11-06 23:57:49 -0600

    Funcion de eliminar incompleta

---
+++ application/views/Vistas/EUV.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Eiminar Usuario</title>
+	<meta charset="utf-8">
+	<br>
+	<h1 align="center" >Eliminar Usuario</h1>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+   		<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+	    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+		<hr>
+</head>
+<body>
+	
+</body>
+</html> 
